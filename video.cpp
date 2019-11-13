5294a87068f6cfb75df73d8c11894739b4d555f3
Margot Besseiche <margot.besseiche@polytechnique.edu>
2019-11-13 12:04:44 +0100

    add rectangle

--- video.cpp
+++ video.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include "video.h"
 #include "image.h"
-#include <opencv2/core.hpp>
-#include <opencv2/video/tracking.hpp>
+#include <opencv2/core/utility.hpp>
+//#include <opencv2/tracking/tracker.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 
 
 
@@ -15,14 +17,14 @@ using namespace cv;
 int open()
 {	// List of tracker types in OpenCV 3.4.1
 	//string trackerTypes[8] = { "BOOSTING", "MIL", "KCF", "TLD","MEDIANFLOW", "GOTURN", "MOSSE", "CSRT" };
-	// vector <string> trackerTypes(types, std::end(types));
+	//vector <string> trackerTypes(types, std::end(types));
 
 	// Create a tracker
 	//string trackerType = trackerTypes[2];
 
-	//Ptr<Tracker> tracker;
+    //Ptr<TrackerKCF> tracker;
 
-	VideoCapture cap("../input/videos/Wildlife.wmv");
+	VideoCapture cap("../input/videos/ShortBasket.mp4");
 
 	// Check if camera opened successfully
 	if (!cap.isOpened())
@@ -33,6 +35,14 @@ int open()
 	int i = 0;
 	cvNamedWindow("Frame", CV_WINDOW_AUTOSIZE);
 	bool ok = true;
+	Rect2d bbox(287, 23, 86, 320);
+	Mat frame;
+
+	// Capture frame-by-frame
+	cap.read(frame);
+
+	// Uncomment the line below to select a different bounding box 
+	bbox = selectROI(frame, false);
 
 		while (1) {
 
@@ -48,6 +58,10 @@ int open()
 				cout << "problem" << endl;
 				break;
 			}
+			 
+			// Display bounding box. 
+			rectangle(frame, bbox, Scalar(255, 0, 0), 2, 1);
+
 
 			// Display the resulting frame
 			imshow("Frame", frame);
